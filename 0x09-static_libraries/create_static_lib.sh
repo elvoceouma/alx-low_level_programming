389289f23fff5595d1eab2a8d73ad5c3f96dde34
Elvice <83223687+elvoceouma@users.noreply.github.com>
2023-09-04 13:25:59 +0300

    updated the create static lib file

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -c *.c
-ar rc liball.a *.o
-ranlib liball.a
+
+# List of source files
+source_files=$(ls *.c)
+
+# Compile each source file into an object file with the corresponding name
+for source_file in $source_files; do
+    object_file="${source_file%.c}.o"
+    gcc -c "$source_file" -o "$object_file"
+done
+
+# Create the static library liball.a from all object files
+ar -rc liball.a *.o
+
+# Clean up: remove the temporary object files
+rm -f *.o
+
